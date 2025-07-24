e3c9e50f26053198030170e22000eb8c0da0fea0
Loki.Nguyen <loki.nguyen@sabawork.com>
2025-07-24 16:41:59 +0400

    add failed test for basic cases

--- RomanNumerals.Tests/RomanTranslatorControllerTest.cs
+++ RomanNumerals.Tests/RomanTranslatorControllerTest.cs
@@ -12,6 +12,11 @@ public class RomanTranslatorControllerTest
     [Test]
     [TestCase(1, "I", Description = "Basic case I when given 1")]
     [TestCase(5, "V", Description = "Basic case V when given 5")]
+    [TestCase(10, "X", Description = "Basic case X when given 10")]
+    [TestCase(50, "L", Description = "Basic case L when given 50")]
+    [TestCase(100, "C", Description = "Basic case C when given 100")]
+    [TestCase(500, "D", Description = "Basic case D when given 500")]
+    [TestCase(1000, "M", Description = "Basic case M when given 1000")]
     public void GetRoman_WhenCall_ReturnsExpectedScore(int input, string expected)
     {
         var controller = new RomanTranslatorController();
