f38cccaa29c8442c7d23af8bfdd4ee78f740c6ce
Loki.Nguyen <loki.nguyen@sabawork.com>
2025-07-24 16:43:03 +0400

    pass basic cases

--- RomanNumerals/Domains/RomanConverter.cs
+++ RomanNumerals/Domains/RomanConverter.cs
@@ -2,7 +2,30 @@ namespace RomanNumerals.Domains;
 
 public class RomanConverter(int numberConstruct)
 {
-    private readonly Dictionary<int, string> _romanDictionary = new() { { 1, "I" }, { 5, "V" } };
+    private readonly Dictionary<int, string> _romanDictionary = new()
+    {
+        {
+            1, "I"
+        },
+        {
+            5, "V"
+        },
+        {
+            10, "X"
+        },
+        {
+            50, "L"
+        },
+        {
+            100, "C"
+        },
+        {
+            500, "D"
+        },
+        {
+            1000, "M"
+        }
+    };
 
     public string ConvertToRomanCharacter()
     {
