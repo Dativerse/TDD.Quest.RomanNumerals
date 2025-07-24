db7828cd581a4ab287ab4a1e6a70c547577a849d
Loki.Nguyen <loki.nguyen@sabawork.com>
2025-07-24 16:39:15 +0400

    refactor: unitest using test cases

--- RomanNumerals.Tests/RomanTranslatorControllerTest.cs
+++ RomanNumerals.Tests/RomanTranslatorControllerTest.cs
@@ -1,5 +1,4 @@
-﻿using NUnit.Framework.Constraints;
-using RomanNumerals.Controllers;
+﻿using RomanNumerals.Controllers;
 
 namespace RomanNumerals.Tests;
 
@@ -11,23 +10,14 @@ public class RomanTranslatorControllerTest
     }
 
     [Test]
-    public void should_be_I_when_given_1()
+    [TestCase(1, "I", Description = "Basic case I when given 1")]
+    [TestCase(5, "V", Description = "Basic case V when given 5")]
+    public void GetRoman_WhenCall_ReturnsExpectedScore(int input, string expected)
     {
         var controller = new RomanTranslatorController();
 
-        var result = controller.GetRoman(1);
+        var result = controller.GetRoman(input);
         
-        Assert.That(result, Is.EqualTo("I"));
-    }
-    
-    
-    [Test]
-    public void should_be_V_when_given_5()
-    {
-        var controller = new RomanTranslatorController();
-
-        var result = controller.GetRoman(5);
-        
-        Assert.That(result, Is.EqualTo("V"));
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
