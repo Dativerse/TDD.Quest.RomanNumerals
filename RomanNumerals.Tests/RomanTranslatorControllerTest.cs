3e0345f9150adf361e0e6249242bfa67eada8184
Loki.Nguyen <loki.nguyen@sabawork.com>
2025-07-24 15:55:42 +0400

    prepair project

---
+++ RomanNumerals.Tests/RomanTranslatorControllerTest.cs
@@ -0,0 +1,15 @@
+﻿namespace RomanNumerals.Tests;
+
+public class RomanTranslatorControllerTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
